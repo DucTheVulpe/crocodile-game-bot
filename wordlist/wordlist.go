d658f30f1f2067041404b526a9922d17197c1286
DucTheVulpe <41674548+DucTheVulpe@users.noreply.github.com>
2024-06-01 23:27:56 +0300

    Create wordlist.go

---
+++ wordlist/wordlist.go
@@ -0,0 +1,41 @@
+package wordlist
+
+import (
+	"io/ioutil"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+var words []string
+
+func Initialize() error {
+	data, err := ioutil.ReadFile("dictionaries/az.txt")
+	if err != nil {
+		return err
+	}
+	words = strings.Fields(string(data))
+	return nil
+}
+
+func Get(index int) string {
+	return words[index]
+}
+
+func Rand() int {
+	return rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(words))
+}
+
+func Prev(index int) int {
+	if index != 0 {
+		index--
+	}
+	return index
+}
+
+func Next(index int) int {
+	if len(words) > index+1 {
+		index++
+	}
+	return index
+}
