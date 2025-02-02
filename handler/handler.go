14306688e65f368bdf99f97c43e2f75379ec71cc
DucTheVulpe <cavidhasilov9@gmail.com>
2025-02-02 17:34:20 +0300

    Create handler.go

---
+++ handler/handler.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	tele "gopkg.in/telebot.v3"
+	"gopkg.in/telebot.v3/layout"
+)
+
+func New(h Handler) handler {
+	return handler{
+		b:   h.Bot,
+		lt:  h.Layout,
+	}
+}
+
+type (
+	Handler struct {
+		Layout *layout.Layout
+		Bot    *tele.Bot
+	}
+
+	handler struct {
+		b   *tele.Bot
+		lt  *layout.Layout
+	}
+)
